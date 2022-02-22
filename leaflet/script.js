b9c2848313b81797fd7906617adbb090580928e4
esuter19 <59849377+esuter19@users.noreply.github.com>
2022-02-21 23:17:22 -0600

    Create script.js

---
+++ leaflet/script.js
@@ -0,0 +1,5 @@
+var map = L.map('mapid').setView([38.635, -90.234], 16);
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+	maxZoom: 19,
+	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
