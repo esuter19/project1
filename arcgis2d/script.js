0ca2768f23b407a026e5b3abd9274ad6790d211a
esuter19 <59849377+esuter19@users.noreply.github.com>
2022-02-22 02:41:02 -0600

    added feature layers

--- arcgis2d/script.js
+++ arcgis2d/script.js
@@ -1,18 +1,66 @@
-require(["esri/Map", "esri/views/MapView", "esri/WebMap"], function (Map, MapView, WebMap) {
-        var map = new Map({
-          basemap: "topo-vector"
+require([
+	  "esri/Map",
+      "esri/views/MapView",
+	  "esri/layers/FeatureLayer",
+      "dojo/domReady!"
+    ], function(
+      Map,MapView, FeatureLayer
+    ) {
+
+  const counties = new FeatureLayer({
+		url:"https://services.arcgis.com/jIL9msH9OI208GCb/ArcGIS/rest/services/USA_Daytime_Population_2016/FeatureServer",
+      outFields: ["*"],
+      popupTemplate: template
+	  });
+  
+	  const hospitals = new FeatureLayer({
+		url:"https://services2.arcgis.com/kNS2ppBA4rwAQQZy/arcgis/rest/services/MO_Hospitals/FeatureServer",
+      outFields: ["*"],
+      popupTemplate: template
+	  });
+
+     
+      var map = new Map({
+          basemap: "streets"
         });
-        
-        var webmap = new WebMap({
-          portalItem: { // autocasts as new PortalItem()
-          id: "e82ef13bd54e4d5f9a4816893d644a18"
-        }
-      });
 
         var view = new MapView({
           container: "viewDiv",
-          map: webmap,
-          zoom: 10,
-          center: [-90.4423392383084,38.62829009276375] // longitude, latitude
+          map: map,
+		  extent: { // autocasts as new Extent()
+			xmin: -88.90255102448988,
+			ymin: 35.75130441496898,
+			xmax: -96.49893502954424,
+			ymax: 40.64652480967901,
+            spatialReference: 4326
+          }
+          
         });
-      });
+    map.add(counties);  
+	  map.add(hospitals);
+	  
+    });
+
+var template = { // autocasts as new PopupTemplate()
+        title: "What Happened?",
+        content: [{
+
+          type: "fields",
+          fieldInfos: [{
+            fieldName: "CollisnTyp",
+            label: "Collision Type",
+            visible: true
+          }]
+        }]
+      };	
+
+var symbol = {
+  type: "simple-marker", 
+  color:"red"
+};
+var renderer = {
+  type: "simple",  // autocasts as new SimpleRenderer()
+  symbol: symbol
+};
+
+//hospitals.renderer = renderer;
